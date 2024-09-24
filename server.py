6439128b3e142ccee76b5518a6a9b4b32bfdbb32
haoyuan-l <haoyuan.li.9654@gmail.com>
2024-09-25 01:06:05 +0200

    fix

--- server.py
+++ server.py
@@ -38,6 +38,7 @@ class Server(fl.server.Server):
             data_loader=self.data_loader,
             num_classes=self.num_classes,
             device=self.device,
+			evaluate_fn=self.get_evaluation_fn()
         )
 
 	def client_manager(self, *args, **kwargs):
