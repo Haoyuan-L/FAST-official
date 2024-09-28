7f99ae4b57f31f05b880ad515140c4dde0cf40af
Haoyuan-L <haoyuan.li.9654@gmail.com>
2024-09-28 03:13:40 +0200

    fix log

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def run_with_different_configs(yaml_config_file):
                                  skewness_alpha=config["skewness_alpha"], class_aware=config["class_aware"], uncertainty=config["uncertainty"])
 
         # Log the results of the experiment
-        fname = config["dataset"]+ "_" + config["data_split"] + "_" + config["uncertainty"] + "_" + "class_aware-" + config["class_aware"]
+        fname = config["dataset"]+ "_" + config["data_split"] + "_" + config["uncertainty"] + "_" + "class_aware-" + str(config["class_aware"])
         log_results(history, config, fname)
 
 
