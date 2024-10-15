a4212d715b4b5a69edcb7a38d147518e1fe08e61
haoyuan-l <haoyuan.li.9654@gmail.com>
2024-10-15 18:57:15 +0200

    update transform

--- data.py
+++ data.py
@@ -159,7 +159,7 @@ def get_data(dataset_name="cifar10", id=0, num_clients=10, return_eval_ds=False,
             transforms.Resize(256, interpolation=transforms.InterpolationMode.BICUBIC),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
-            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            transforms.Normalize([0.5], [0.5]),
         ])
     model.to(device)
     model.eval()
